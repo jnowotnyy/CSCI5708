c86c0cad4ca4f8c27ee54729951d6f591fabbbe1
Jason Nowotny <nowot005@umn.edu>
2021-02-06 20:11:40 -0600

    Q4 Finalized

--- Homework1/CSCI5708_HMWK1_Q4.sql
+++ Homework1/CSCI5708_HMWK1_Q4.sql
@@ -5,88 +5,89 @@ set search_path to 'registration';
 
 --Entities
 create table if not exists Student (
-Email CHAR(20),
+StudentEmail CHAR(20),
 Name CHAR(50),
 GPA REAL,
-constraint pk_Student primary key (Email)
-);
-
-create table if not exists Graduate_Student (
-Graduate_Email CHAR(20),
-Year CHAR(10),
-constraint pk_Graduate primary key (Graduate_Email),
-constraint fk_Graduate foreign key (Graduate_Email) references Student(Email)
-);
-
-create table if not exists Undergraduate_Student (
-Undergrad_Email CHAR(20),
-Research_area CHAR(10),
-constraint pk_Undergraduate primary key (Undergrad_Email),
-constraint fk_Undergraduate foreign key (Undergrad_Email) references Student(Email)
+constraint pk_Student primary key (StudentEmail)
 );
 
 create table if not exists Course (
-id CHAR(20),
+CourseId CHAR(20),
 Name CHAR(50),
 Credits INTEGER,
-constraint pk_Course primary key (id)
+constraint pk_Course primary key (CourseId)
 );
 
 create table if not exists Professor (
-Email CHAR(20),
+ProfEmail CHAR(20),
 Name CHAR(50),
-constraint pk_Professor primary key (Email)
+constraint pk_Professor primary key (ProfEmail)
 );
 
 create table if not exists Category (
-id CHAR(50),
+CategoryId CHAR(50),
 Name CHAR(50),
-constraint pk_Category primary key (id)
+constraint pk_Category primary key (CategoryId)
+);
+
+create table if not exists Graduate_Student (
+GraduateEmail CHAR(20),
+Year CHAR(10),
+constraint pk_Graduate primary key (GraduateEmail),
+constraint fk_Graduate foreign key (GraduateEmail) references Student(StudentEmail)
+);
+
+create table if not exists Undergraduate_Student (
+UndergradEmail CHAR(20),
+Research_area CHAR(10),
+constraint pk_Undergraduate primary key (UndergradEmail),
+constraint fk_Undergraduate foreign key (UndergradEmail) references Student(StudentEmail)
 );
 
 --Relations
 
 create table if not exists Registers (
-Email CHAR(20),
-id CHAR(20),
+StudentEmail CHAR(20),
+CourseId CHAR(20),
 Semester CHAR(20),
-constraint pk_Registers primary key (Email, id),
-constraint fk_Student foreign key (Email) references Student(Email),
-constraint fk_Course foreign key (id) references Course(id)
+constraint pk_Registers primary key (StudentEmail, CourseId),
+constraint fk_Student foreign key (StudentEmail) references Student(StudentEmail),
+constraint fk_Course foreign key (CourseId) references Course(CourseId)
 );
 
 create table if not exists Teaches (
-Email CHAR(20),
-id CHAR(20),
-constraint pk_Registers primary key (id),
-constraint fk_Professor foreign key (Email) references Professor(Email) on delete cascade, --key constraint cascade if professor removed?
-constraint fk_Course foreign key (id) references Course(id)
+ProfEmail CHAR(20),
+Courseid CHAR(20),
+constraint pk_Teaches primary key (Courseid),
+constraint fk_Professor foreign key (ProfEmail) references Professor(ProfEmail),
+constraint fk_Course foreign key (CourseId) references Course(CourseId)
 );
 
-create table if not exists Classifies(
-Course_id CHAR(20),
-Category_id CHAR(20),
+
+create table if not exists Classifies (
+CourseId CHAR(20),
+CategoryId CHAR(20),
 Name CHAR(50),
-constraint pk_Classifies primary key (Course_id, Category_id),
-constraint fk_Course foreign key (Course_id) references Course(id),
-constraint fk_Category foreign key (Category_id) references Category(id)
+constraint pk_Classifies primary key (CourseId, CategoryId),
+constraint fk_Course foreign key (CourseId) references Course(CategoryId),
+constraint fk_Category foreign key (CategoryId) references Category(CategoryId)
 );
 
 create table if not exists Categorizes (
-SuperCategory_id CHAR(20),
-SubCategory_id CHAR(20),
-constraint pk_Classifies primary key (SuperCategory_id, SubCategory_id),
-constraint fk_SuperCategory foreign key (SuperCategory_id) references Category(id),
-constraint fk_SubCategory foreign key (SubCategory_id) references Category(id)
+SuperCategoryId CHAR(20),
+SubCategoryId CHAR(20),
+constraint pk_Classifies primary key (SuperCategoryId, SubCategoryId),
+constraint fk_SuperCategory foreign key (SuperCategoryId) references Category(CategoryId),
+constraint fk_SubCategory foreign key (SubCategoryId) references Category(CategoryId)
 );
 
 create table if not exists Advises (
 Research_area CHAR(10),
-Professor_Email CHAR(20),
-Graduate_Email CHAR(20),
-constraint pk_Advises primary key (Graduate_Email),
-constraint fk_Graduate foreign key (Graduate_Email) references Graduate_Student(Graduate_Email),
-constraint fk_Professor foreign key (Professor_Email) references Professor(Email) on delete cascade --key constraint cascade if professor removed?
+ProfEmail CHAR(20),
+GraduateEmail CHAR(20),
+constraint pk_Advises primary key (GraduateEmail),
+constraint fk_Graduate foreign key (GraduateEmail) references Graduate_Student(GraduateEmail),
+constraint fk_Professor foreign key (ProfEmail) references Professor(ProfEmail) 
 );
 
 
