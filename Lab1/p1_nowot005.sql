a71666d46ca6768490ee54017ae1ddaf0f8fcce4
Jason Nowotny <nowot005@umn.edu>
2021-02-06 21:32:06 -0600

    updates to p1

--- Lab1/p1_nowot005.sql
+++ Lab1/p1_nowot005.sql
@@ -3,19 +3,19 @@ create schema if not exists ProjectDB;
 
 set search_path to 'ProjectDB';
 
-create table if not exists University (
+create table if not exists ProjectDB.University (
 UnivId NUMERIC,
 UnivName VARCHAR(40),
 constraint pk_University PRIMARY KEY (UnivId)
 );
 
-create table if not exists Department (
+create table if not exists ProjectDB.Department (
 DeptId NUMERIC,
 DeptName VARCHAR(40),
 constraint pk_Department PRIMARY KEY (DeptId)
 );
 
-create table if not exists Employee (
+create table if not exists ProjectDB.Employee (
 EmpId NUMERIC,
 EmpName VARCHAR(40),
 DeptId NUMERIC,
@@ -24,13 +24,13 @@ constraint pk_Employee PRIMARY KEY (EmpId),
 constraint fk_Department FOREIGN KEY (DeptId) REFERENCES Department(DeptId)
 );
 
-create table if not exists Project (
+create table if not exists ProjectDB.Project (
 ProjId NUMERIC,
 ProjName VARCHAR(40),
 constraint pk_Project PRIMARY KEY (ProjId)
 );
 
-create table if not exists Graduate (
+create table if not exists ProjectDB.Graduate (
 EmpId NUMERIC,
 UnivId NUMERIC,
 GradYear NUMERIC,
@@ -39,7 +39,7 @@ constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES Employee(EmpId),
 constraint fk_University FOREIGN KEY (UnivId) REFERENCES University(UnivId)
 );
 
-create table if not exists EmpProject (
+create table if not exists ProjectDB.EmpProject (
 EmpId NUMERIC,
 ProjId NUMERIC,
 constraint pk_EmpProject PRIMARY KEY (EmpId, ProjId),
@@ -47,7 +47,7 @@ constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES Employee(EmpId),
 constraint fk_Project FOREIGN KEY (ProjId) REFERENCES Project(ProjId)
 )
 
-create table if not exists ProjectManager (
+create table if not exists ProjectDB.ProjectManager (
 ProjId NUMERIC,
 MgrId NUMERIC,
 constraint pk_ProjectManager PRIMARY KEY (ProjId, MgrId),
