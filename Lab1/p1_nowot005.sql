400235354f0e020295eb1e10c36ccef89a893d98
Jason Nowotny <nowot005@umn.edu>
2021-02-06 21:41:30 -0600

    updates to p1

--- Lab1/p1_nowot005.sql
+++ Lab1/p1_nowot005.sql
@@ -21,7 +21,7 @@ EmpName VARCHAR(40),
 DeptId NUMERIC,
 HomeZipCode NUMERIC,
 constraint pk_Employee PRIMARY KEY (EmpId),
-constraint fk_Department FOREIGN KEY (DeptId) REFERENCES Department(DeptId)
+constraint fk_Department FOREIGN KEY (DeptId) REFERENCES ProjectDB.Department(DeptId)
 );
 
 create table if not exists ProjectDB.Project (
@@ -35,22 +35,22 @@ EmpId NUMERIC,
 UnivId NUMERIC,
 GradYear NUMERIC,
 constraint pk_Graduate PRIMARY KEY (EmpId),
-constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES Employee(EmpId),
-constraint fk_University FOREIGN KEY (UnivId) REFERENCES University(UnivId)
+constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES ProjectDB.Employee(EmpId),
+constraint fk_University FOREIGN KEY (UnivId) REFERENCES ProjectDB.University(UnivId)
 );
 
 create table if not exists ProjectDB.EmpProject (
 EmpId NUMERIC,
 ProjId NUMERIC,
 constraint pk_EmpProject PRIMARY KEY (EmpId, ProjId),
-constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES Employee(EmpId),
-constraint fk_Project FOREIGN KEY (ProjId) REFERENCES Project(ProjId)
-)
+constraint fk_Employee FOREIGN KEY (EmpId) REFERENCES ProjectDB.Employee(EmpId),
+constraint fk_Project FOREIGN KEY (ProjId) REFERENCES ProjectDB.Project(ProjId)
+);
 
 create table if not exists ProjectDB.ProjectManager (
 ProjId NUMERIC,
 MgrId NUMERIC,
 constraint pk_ProjectManager PRIMARY KEY (ProjId, MgrId),
-constraint fk_Project FOREIGN KEY (ProjId) REFERENCES Project(ProjId),
-constraint fk_Manager FOREIGN KEY (MgrId) REFERENCES Employee(EmpId)
-)
+constraint fk_Project FOREIGN KEY (ProjId) REFERENCES ProjectDB.Project(ProjId),
+constraint fk_Manager FOREIGN KEY (MgrId) REFERENCES ProjectDB.Employee(EmpId)
+);
