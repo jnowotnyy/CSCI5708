02747037c554dc21b078e92bfb777c3311dde85c
Jason Nowotny <nowot005@umn.edu>
2021-02-11 11:01:14 -0600

    add data_corrected and p3

---
+++ Lab1/data_corrected.sql
@@ -0,0 +1,31 @@
+INSERT INTO ProjectDB.University VALUES (1, 'University of Minnesota');
+INSERT INTO ProjectDB.University VALUES (2, 'University of Wisconsin');
+INSERT INTO ProjectDB.University VALUES (3, 'University of Washington');
+
+INSERT INTO ProjectDB.Department VALUES (1, 'Development');
+INSERT INTO ProjectDB.Department VALUES (2, 'UX Design');
+INSERT INTO ProjectDB.Department VALUES (3, 'Technical Architects');
+
+INSERT INTO ProjectDB.Employee VALUES (1, 'James', 1, 55414);
+INSERT INTO ProjectDB.Employee VALUES (2, 'Michael', 1, 55455);
+INSERT INTO ProjectDB.Employee VALUES (3, 'Lin', 2, 55414);
+INSERT INTO ProjectDB.Employee VALUES (4, 'Rafel', 3, 55415);
+
+INSERT INTO ProjectDB.Project VALUES (1, 'Mobile');
+INSERT INTO ProjectDB.Project VALUES (2, 'UTC');
+INSERT INTO ProjectDB.Project VALUES (3, 'CD Finance');
+
+INSERT INTO ProjectDB.Graduate VALUES (1, 1, 1995);
+INSERT INTO ProjectDB.Graduate VALUES (2, 1, 2012);
+INSERT INTO ProjectDB.Graduate VALUES (3, 2, 1999);
+INSERT INTO ProjectDB.Graduate VALUES (4, 3, 2001);
+
+INSERT INTO ProjectDB.EmpProject VALUES(1, 1);
+INSERT INTO ProjectDB.EmpProject VALUES(1, 1);
+INSERT INTO ProjectDB.EmpProject VALUES(2, 1);
+INSERT INTO ProjectDB.EmpProject VALUES(3, 2);
+
+INSERT INTO ProjectDB.ProjectManager VALUES(1, 1);
+INSERT INTO ProjectDB.ProjectManager VALUES(1, 2);
+INSERT INTO ProjectDB.ProjectManager VALUES(2, 3);
+INSERT INTO ProjectDB.ProjectManager VALUES(3, 2);
--- Lab1/p2_nowot005.sql
+++ Lab1/p2_nowot005.sql
@@ -1,6 +1,50 @@
 --#1
-SELECT Emp.EmpName
-FROM Employee Emp
-WHERE Emp.HomeZipCode = 55414 OR Emp.HomeZipCode = 55455
+--Find the names of the employees who are living in Minneapolis (Zip code 55414 or 55455).
+
+SELECT E.EmpName
+FROM ProjectDB.Employee E
+WHERE E.HomeZipCode = 55414 
+    OR E.HomeZipCode = 55455;
 
 --#2
+--Find the names of the projects that are currently managed by any manager
+SELECT DISTINCT P.ProjName
+FROM ProjectDB.ProjectManager PM, ProjectDB.Project P
+WHERE PM.ProjId = P.ProjId 
+    AND PM.MgrId IS NOT NULL;
+
+--#3
+--For each project, display its name as well as the number of employees who are currently
+--working on it except for a project without any employees.
+SELECT P.ProjName, COUNT(EP.EmpId)
+FROM ProjectDB.Project P, ProjectDB.EmpProject EP
+WHERE P.ProjId = EP.ProjId
+GROUP BY P.ProjName;
+
+--#4
+--Find the name(s) of the university/universities that graduated the maximum number of
+--distinct managers.
+--SELECT U.UnivName
+--FROM ProjectDB.University U
+--WHERE U.UnivID 
+    --IN (SELECT G.UnivId
+        --FROM ProjectDB.Graduate G
+        --WHERE G.EmpId 
+            --IN (SELECT PM.EmpId, MAX(SELECT COUNT(M))))
+
+--#5
+--For each employee, say E, display the name of E, the department name of E, and the
+--graduation year of E.
+SELECT E.EmpName, D.DeptName, G.GradYear
+FROM ProjectDB.Employee E, ProjectDB.Department D, ProjectDB.Graduate G
+WHERE E.DeptId = D.DeptId 
+    AND E.EmpId = G.EmpId;
+
+--#6
+--Display the name of the project that has the maximum number of different employees
+--who worked/”are working” on it. If more than one project qualify, display all the qualified
+--projects. (Hint: refer to EmpProject only and not ProjectManager).
+--SELECT P.ProjName
+--FROM ProjectDB.Project P
+--WHERE ProjId = (SELECT MAX(EP.EmpId)
+--                FROM ProjectDB.EmpProject EP);
